7849949094c7a58cf9cdd27c0186d9323daa58c6
Jeff Hykin <jeffhykin@jeffs-macbookpro.lan>
2019-09-19 18:33:06 -0500

    _finish coffee switch

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const path = require("path")
+
+module.exports = {
+    runtimeCompiler: true,
+    chainWebpack: config => {
+        config
+            .entry("app")
+            .clear()
+            .add("./src/main.coffee")
+            .end()
+        config.resolve.alias.set("@", path.join(__dirname, "./src"))
+    },
+}
